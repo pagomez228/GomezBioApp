9351335af1d5788993d2ecff6b46dc83a39e37a1
Phillip Gomez <pagomez228@gmail.com>
2018-10-29 18:14:53 -0500

    Fixed ContactMe page text. Edited Skills page to only have a list view with text.

--- GomezBioApp/GomezBioApp/SkillsPage.xaml.cs
+++ GomezBioApp/GomezBioApp/SkillsPage.xaml.cs
@@ -13,18 +13,22 @@ namespace GomezBioApp
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class SkillsPage : ContentPage
 	{
-        public ObservableCollection<string> Skills { get; set; }
+        public ObservableCollection<Skill> Skills { get; set; }
 		public SkillsPage ()
 		{
 			InitializeComponent ();
-            this.Skills = new ObservableCollection<string>();
+            this.Skills = new ObservableCollection<Skill>();
+            this.Skills.Add(new Skill {Icon = "javaIcon.png", Description = "Java Programming Basics", Name = "Java" });
+            this.Skills.Add(new Skill {Icon = "xamrinIcon.png", Description = "Xamarin Forms Basics", Name = "Xamarin Forms" });
+            this.Skills.Add(new Skill {Icon = "carIcon.png", Description = "8 years driving experience", Name = "Driving Experience" });
+            skillsListView.ItemsSource = this.Skills;
 		}
 
-        private void Button_Clicked(object sender, EventArgs e)
+        public class Skill
         {
-            this.Skills.Add(skillEntry.Text);
-            skillEntry.Text = "";
-            skillsListView.ItemsSource = this.Skills;
+            public string Icon { get; set; }
+            public string Description { get; set; }
+            public string Name { get; set; }
         }
     }
 }
